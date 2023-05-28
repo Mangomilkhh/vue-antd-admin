198d388363117a0b0367904bf502667ad205f34f
hiannna <908046710@qq.com>
2023-05-28 15:20:18 +0800

    最终确定可编辑单元格、测试增加对话框里日期选择器getCalendarContainer结果使用正常

--- src/pages/detail/SelfTable.vue
+++ src/pages/detail/SelfTable.vue
@@ -1,160 +1,255 @@
 <template>
-	<page-layout title="自定义表格">
-		<a-card :bordered="false"  v-if="showComponent">
-			<div>
-				<a-button @click="handleAdd">Add</a-button>
-				<a-button v-if="editable" @click="check">提交</a-button>
-				<a-button v-if="editable" @click="cancel">取消</a-button>
-				<a-button v-else @click="editable = true" type="primary">批量修改</a-button>
-				<a-table bordered :data-source="dataSource" :columns="columns" :key="componentKey">
-					<template slot="name" slot-scope="text, record">
-						<editable-cell :text="text" :editable="editable" @change="onCellChange(record.key, 'name', $event)" />
-					</template>
-					<template slot="address" slot-scope="text, record">
-						<editable-cell :text="text" :editable="editable" @change="onCellChange(record.key, 'address', $event)" />
-					</template>
-					<template slot="operation" slot-scope="text, record">
-						<a-popconfirm v-if="dataSource.length" title="Sure to delete?" @confirm="() => onDelete(record.key)">
-							<a href="javascript:;">Delete</a>
-						</a-popconfirm>
-					</template>
-				</a-table>
-			</div>
-		</a-card>
-	</page-layout>
+  <page-layout title="修改：可编辑单元格">
+    <a-card :bordered="false" v-if="showComponent">
+      <div>
+        <a-button @click="handleAdd">Add</a-button>
+        <a-button v-if="editable" @click="check">提交</a-button>
+        <a-button v-if="editable" @click="cancel">取消</a-button>
+        <a-button v-else @click="editable = true" type="primary"
+          >批量修改</a-button
+        >
+        <a-table
+          bordered
+          :data-source="dataSource"
+          :columns="columns"
+          :key="componentKey"
+        >
+          <div
+            v-for="col in ['name', 'age', 'address']"
+            :key="col"
+            :slot="col"
+            slot-scope="text, record"
+          >
+            <editable-cell
+              :text="text"
+              :editable="editable"
+              @change="onCellChange(record.key, col, $event)"
+            />
+          </div>
+          <template slot="operation" slot-scope="text, record">
+            <a href="javascript:;" @click="editRow(record)">编辑</a>
+          </template>
+        </a-table>
+      </div>
+    </a-card>
+
+    <!-- 弹出对话框 -->
+    <a-modal
+      title="Title"
+      :visible="showModal"
+      @ok="handleOk"
+      @cancel="handleOk"
+    >
+      <!-- <p>{{ modalText }}</p> -->
+      <!--  @submit="handleSubmit" -->
+      <a-form :form="form" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
+        <a-form-item label="Note">
+          <a-input
+            v-decorator="[
+              'note',
+              {
+                rules: [{ required: true, message: 'Please input your note!' }],
+              },
+            ]"
+          />
+        </a-form-item>
+        <a-form-item label="Gender">
+          <a-select
+            :getPopupContainer="(trigger) => trigger.parentNode"
+            v-decorator="[
+              'gender',
+              {
+                rules: [
+                  { required: true, message: 'Please select your gender!' },
+                ],
+              },
+            ]"
+          >
+            <a-select-option value="male"> male </a-select-option>
+            <a-select-option value="female"> female </a-select-option>
+          </a-select>
+        </a-form-item>
+        <a-form-item label="日期："
+          ><a-date-picker
+            :getCalendarContainer="(trigger) => trigger.parentNode"
+        /></a-form-item>
+        <a-form-item label="日期："
+          ><a-date-picker
+            :getCalendarContainer="(trigger) => trigger.parentNode"
+        /></a-form-item>
+        <a-form-item label="日期："
+          ><a-date-picker
+            :getCalendarContainer="(trigger) => trigger.parentNode"
+        /></a-form-item>
+        <a-form-item label="日期："
+          ><a-date-picker
+            :getCalendarContainer="(trigger) => trigger.parentNode"
+        /></a-form-item>
+        <a-form-item label="日期："
+          ><a-date-picker
+            :getCalendarContainer="(trigger) => trigger.parentNode"
+        /></a-form-item>
+        <a-form-item label="日期："
+          ><a-date-picker
+            :getCalendarContainer="(trigger) => trigger.parentNode"
+        /></a-form-item>
+        <a-form-item label="日期："
+          ><a-date-picker
+            :getCalendarContainer="(trigger) => trigger.parentNode"
+        /></a-form-item>
+      </a-form>
+    </a-modal>
+  </page-layout>
 </template>
 
 <script>
 import PageLayout from "../../layouts/PageLayout";
-import EditableCell from "./EditableCell.vue"
+import EditableCell from "./EditableCell.vue";
 
 export default {
-	name: "SelfTable",
-	components: {
-		EditableCell, PageLayout
-	},
-	inject: ['selfRefresh'],//TabsView祖先文件provide过来的方法
-	data() {
-		return {
-			componentKey:0,
-			showComponent: true,
-			editable: false,
-			dataSource: [
-				{
-					key: '0',
-					name: 'zero',
-					age: '3',
-					address: '',
-				},
-				{
-					key: '1',
-					name: 'one',
-					age: '32',
-					address: 'Londonone',
-				},
-			],
-			dataSource2: [],
-			count: 2,
-			columns: [
-				{
-					title: 'name',
-					dataIndex: 'name',
-					width: '30%',
-					align: "center",
-					scopedSlots: { customRender: 'name' },
-				},
-				{
-					title: 'age',
-					dataIndex: 'age',
-				},
-				{
-					title: 'address',
-					dataIndex: 'address',
-					width: '30',
-					scopedSlots: { customRender: 'address' },
-				},
-				{
-					title: 'operation',
-					dataIndex: 'operation',
-					scopedSlots: { customRender: 'operation' },
-				},
-			],
-		};
-	},
-	methods: {
-		check() {
-			this.editable = false;
+  name: "SelfTable",
+  components: {
+    EditableCell,
+    PageLayout,
+  },
+  inject: ["selfRefresh"], //TabsView祖先文件provide过来的方法
+  data() {
+    return {
+      componentKey: 0,
+      showComponent: true,
+      editable: false,
+      dataSource: [
+        {
+          key: "0",
+          name: "zero",
+          age: "3",
+          address: "",
+        },
+        {
+          key: "1",
+          name: "one",
+          age: "32",
+          address: "Londonone",
+        },
+      ],
+      dataSource2: [],
+      count: 2,
+      columns: [
+        {
+          title: "name",
+          dataIndex: "name",
+          width: "30%",
+          align: "center",
+          scopedSlots: { customRender: "name" },
+        },
+        {
+          title: "age",
+          dataIndex: "age",
+          scopedSlots: { customRender: "address" },
+        },
+        {
+          title: "address",
+          dataIndex: "address",
+          width: "30",
+          scopedSlots: { customRender: "address" },
+        },
+        {
+          title: "operation",
+          dataIndex: "operation",
+          scopedSlots: { customRender: "operation" },
+        },
+      ],
+      showModal: false,
+      modalText: "",
+      formLayout: "horizontal",
+      form: this.$form.createForm(this, { name: "coordinated" }),
+    };
+  },
+  methods: {
+    editRow(record) {
+      this.showModal = true;
+      this.modalText = record;
+    },
+    handleOk() {
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log("Received values of form: ", values);
+        }
+      });
+      this.showModal = false;
+    },
+    check() {
+      this.editable = false;
       //提交this.dataSource给接口再刷新
 
-			//把组件设置显示隐藏的方式刷新
-			// this.showComponent = false
+      //把组件设置显示隐藏的方式刷新
+      // this.showComponent = false
       // this.$nextTick(() => {
       //   this.showComponent = true
       // })
 
-			//给组件的key增加的方式刷新,视图才可以显示最新值
-			// this.componentKey += 1;
-		},
-		cancel() {
-			this.editable = false;
-      //试试重新刷新接口,获取初始值
+      //给组件的key增加的方式刷新,视图才可以显示最新值
+      // this.componentKey += 1;
+    },
+    cancel() {
+      this.editable = false;
+      // 重新刷新接口的函数获取最新值后赋给表格dataSource
 
       // this.dataSource=(接口的初始值)
 
-			// window.location.reload();//会刷新这个网页，性能不好
-			// this.selfRefresh('selftable_cancel'//刷新当前组件
-			// 	, {
-			// 		cachedKey: "/details/selftable163",
-			// 		// fullPath: "/details/selftable",
-			// 		// keyPath: "/details/selftable",
-			// 		loading: false,
-			// 		path: "/details/selftable",
-			// 		_init_: true
-			// 	})
-		},
-		onCellChange(key, dataIndex, value) {
+      // window.location.reload();//会刷新这个网页，性能不好
+      // this.selfRefresh('selftable_cancel'//刷新当前组件
+      // 	, {
+      // 		cachedKey: "/details/selftable163",
+      // 		// fullPath: "/details/selftable",
+      // 		// keyPath: "/details/selftable",
+      // 		loading: false,
+      // 		path: "/details/selftable",
+      // 		_init_: true
+      // 	})
+    },
+    onCellChange(key, dataIndex, value) {
       //数据一直实时更新，就看视图怎么显示
-			// const dataSource2 = [...this.dataSource];
-			const dataSource2 = JSON.parse(JSON.stringify([...this.dataSource]));
-			const target = dataSource2.find(item => item.key === key);
-			if (target) {
-				target[dataIndex] = value;
-				this.dataSource = dataSource2;
-			}
-			// console.log('100',key, dataIndex, value);
-			// console.log('3213', this.dataSource, dataSource2);
-		},
-		onDelete() {
-			// const dataSource = [...this.dataSource];
-			// this.dataSource = dataSource.filter(item => item.key !== key);
-		},
-		handleAdd() {
-			// const { count, dataSource } = this;
-			// const newData = {
-			// 	key: count,
-			// 	name: `Edward King ${count}`,
-			// 	age: 32,
-			// 	address: `London, Park Lane no. ${count}`,
-			// };
-			// this.dataSource = [...dataSource, newData];
-			// this.count = count + 1;
-		},
-	},
+      const dataSource2 = [...this.dataSource];
+      const target = dataSource2.find((item) => item.key === key);
+      if (target) {
+        target[dataIndex] = value;
+        this.dataSource = dataSource2;
+      }
+      console.log('100',key, dataIndex, value);
+      // console.log('3213', this.dataSource, dataSource2);
+    },
+    onDelete() {
+      // const dataSource = [...this.dataSource];
+      // this.dataSource = dataSource.filter(item => item.key !== key);
+    },
+    handleAdd() {
+      // const { count, dataSource } = this;
+      // const newData = {
+      // 	key: count,
+      // 	name: `Edward King ${count}`,
+      // 	age: 32,
+      // 	address: `London, Park Lane no. ${count}`,
+      // };
+      // this.dataSource = [...dataSource, newData];
+      // this.count = count + 1;
+    },
+  },
 };
 </script>
 
 
 <style>
-.editable-cell {
-	/* position: relative; */
-}
+/* .editable-cell {
+  position: relative;
+} */
 
 .editable-cell-text-wrapper {
-	padding: 5px 24px 5px 5px;
+  padding: 5px 24px 5px 5px;
 }
 
 .ant-btn {
-	margin: 0 10px 10px 0;
+  margin: 0 10px 10px 0;
 }
 </style>
   
--- src/pages/detail/SelfTable2.vue
+++ src/pages/detail/SelfTable2.vue
@@ -1,63 +1,50 @@
 <template>
-  <a-table :columns="columns" :data-source="data" bordered>
-    <div
-      v-for="col in editColumns"
-      :slot="col"
-      slot-scope="text, record"
-      :key="col"
-    >
-      <div style="float: left; margin-right: 0">
-        <a-input
-          v-if="record.editable"
-          style="margin: -5px 0"
-          :value="text"
-          @change="(e) => handleChange(e.target.value, record.key, col)"
-        />
-        <template v-else>
-          {{ text }}
-        </template>
+  <page-layout title="修改：可编辑行">
+    <a-card :bordered="false">
+      <div v-if="editInput">
+        <a-button @click="() => save()">确定</a-button>
+        <a-button @click="() => cancel()">取消</a-button>
       </div>
-      <div style="float: right">
-        <span v-if="record.editable">
-          <a
-            style="
-              color: #2eabff;
-              fontsize: 15px;
-              margin-left: 0;
-              margin-right: 20px;
-            "
-            @click="() => save(record.key)"
-            >确定</a
-          >
-          <a-popconfirm
-            title="确认取消重命名?"
-            @confirm="() => cancel(record.key)"
-          >
-            <a type="close" style="color: #2eabff; fontsize: 15px">取消</a>
-          </a-popconfirm>
-        </span>
-        <span v-else>
-          <a-icon
-            style="color: #2eabff; fontsize: 20px"
-            type="edit"
-            :disabled="editingKey !== ''"
-            @click="() => edit(record.key)"
-          />
-        </span>
-      </div>
-    </div>
+      <a-button v-else @click="editInput = true">批量编辑</a-button>
 
-    <template slot="operation" slot-scope="text, record">
-      <a-popconfirm
-        title="Sure to delete?"
-        @confirm="() => onDelete(record.key)"
-      >
-        <a href="javascript:;">Delete</a>
-      </a-popconfirm>
-    </template>
-  </a-table>
+      <a-table :columns="columns" :data-source="data" bordered>
+        <div
+          v-for="col in ['name', 'age', 'address']"
+          :slot="col"
+          slot-scope="text, record"
+          :key="col"
+        >
+          <div>
+            <a-input
+            :key="record.key"
+              id="myInput"
+              v-if="editInput"
+              :value="text"
+              @change="
+                (e) => handleChange(text, e.target.value, record.key, col)
+              "
+              :style="changeStyle ? 'color: #ee4d2d' : 'color: #000000a6'"
+            />
+            <template v-else>
+              {{ text }}
+            </template>
+          </div>
+        </div>
+
+        <template slot="operation" slot-scope="text, record">
+          <a-popconfirm
+            title="Sure to delete?"
+            @confirm="() => onDelete(record.key)"
+          >
+            <a href="javascript:;">Delete</a>
+          </a-popconfirm>
+        </template>
+      </a-table>
+    </a-card>
+  </page-layout>
 </template>
 <script>
+import PageLayout from "../../layouts/PageLayout";
 const columns = [
   {
     title: "name",
@@ -84,69 +71,57 @@ const columns = [
   },
 ];
 
-const data = [];
-for (let i = 0; i < 100; i++) {
-  data.push({
-    key: i.toString(),
-    name: `Edrward ${i}`,
-    age: 32,
-    address: `London Park no. ${i}`,
-  });
-}
 export default {
+  components: {
+    PageLayout,
+  },
   data() {
-    this.cacheData = data.map((item) => ({ ...item }));
     return {
-      data,
+      data: [],
+      cacheData: [],
       columns,
-      editingKey: "",
-      editColumns: ["name", "age", "address"], //自行扩展即可
+      editInput: false,
+      changeStyle: false,
     };
   },
+  created() {
+    this.initData();
+  },
   methods: {
-    handleChange(value, key, column) {
-      const newData = [...this.data];
+    initData() {
+      for (let i = 0; i < 100; i++) {
+        this.data.push({
+          key: i.toString(),
+          name: `Edrward ${i}`,
+          age: "",
+          address: `London Park no. ${i}`,
+        });
+      }
+      // this.cacheData = data.map((item) => ({ ...item })); //创建副本，始终为初始值
+      this.cacheData = JSON.parse(JSON.stringify(this.data));
+    },
+    handleChange(text, value, key, column) {
+      this.changeStyle=true,
+      
+
+      //值  行  列
+      console.log("单元格更改", value, key, column);
+      const newData = this.data;
       const target = newData.filter((item) => key === item.key)[0];
       if (target) {
         target[column] = value;
         this.data = newData;
       }
     },
-    edit(key) {
-      const newData = [...this.data];
-      const target = newData.filter((item) => key === item.key)[0];
-      this.editingKey = key;
-      if (target) {
-        target.editable = true;
-        this.data = newData;
-      }
+    save() {
+      this.editInput = false;
+      // const aaa= this.data;
+      //调接口保存aaa
     },
-    save(key) {
-      const newData = [...this.data];
-      const newCacheData = [...this.cacheData];
-      const target = newData.filter((item) => key === item.key)[0];
-      console.log("target:", JSON.stringify(target));
-      const targetCache = newCacheData.filter((item) => key === item.key)[0];
-      if (target && targetCache) {
-        delete target.editable;
-        this.data = newData;
-        Object.assign(targetCache, target);
-        this.cacheData = newCacheData;
-      }
-      this.editingKey = "";
-    },
-    cancel(key) {
-      const newData = [...this.data];
-      const target = newData.filter((item) => key === item.key)[0];
-      this.editingKey = "";
-      if (target) {
-        Object.assign(
-          target,
-          this.cacheData.filter((item) => key === item.key)[0]
-        );
-        delete target.editable;
-        this.data = newData;
-      }
+    cancel() {
+      this.editInput = false;
+      this.data = JSON.parse(JSON.stringify(this.cacheData));
+      // 重新刷新接口的函数获取最新值后赋给表格
     },
   },
 };
