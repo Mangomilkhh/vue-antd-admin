df02871af4bf1c9a00863a605adde4d775ecb31d
Mangomilkhh <908046710@qq.com>
2023-11-15 23:32:35 +0800

    优化性能，修改批量编辑为点击内容可编辑当前行

--- src/pages/detail/SelfTable2.vue
+++ src/pages/detail/SelfTable2.vue
@@ -7,7 +7,17 @@
       </div>
       <a-button v-else @click="editInput = true">批量编辑</a-button>
 
-      <a-table :columns="columns" :data-source="data" rowKey="name" bordered>
+      <a-table
+        :columns="columns"
+        :data-source="data"
+        rowKey="name"
+        bordered
+        :pagination="{
+          showSizeChanger: true,
+          showQuickJumper: true,
+          pageSize: 200,
+        }"
+      >
         <!--   ['name', 'age', 'address'] -->
         <span
           v-for="(col, index) in columns"
@@ -15,14 +25,59 @@
           slot-scope="text, record"
           :key="index"
         >
-          <a-input
-            :key="record.key"
-            id="myInput"
-            v-if="editInput"
-            :value="text"
-            @change="(e) => handleChange(text, e.target.value, record.key, col)"
-            :style="changeStyle ? 'color: #ee4d2d' : 'color: #000000a6'"
-          />
+          <span v-if="editInput">
+            <div v-if="col.type == 'input'">
+              <span v-if="record.disable_edit" style="color: #ccc"
+                >{{ text }}
+              </span>
+              <span v-else>
+                <span
+                  v-show="record.change_status"
+                  @click="changeEdit(record)"
+                  >{{ text }}</span
+                >
+
+                <a-input
+                  v-show="!record.change_status"
+                  :key="record.key"
+                  id="myInput"
+                  :value="text"
+                  @change="
+                    (e) => handleChange(text, e.target.value, record.key, col)
+                  "
+                  :style="changeStyle ? 'color: #ee4d2d' : 'color: #000000a6'"
+              /></span>
+            </div>
+
+            <div v-if="col.type == 'multiple_select'">
+              <span v-if="record.disable_edit" style="color: #ccc"
+                >{{ text }}
+              </span>
+              <span v-else>
+                <span
+                  v-show="record.change_status"
+                  @click="changeEdit(record)"
+                  >{{ text }}</span
+                >
+                <a-select
+                  v-show="!record.change_status"
+                  style="width: 100%"
+                  :value="text"
+                  mode="tags"
+                  @change="
+                    (value) => selectChange(text, value, record.key, col)
+                  "
+                >
+                  <a-select-option
+                    v-for="item in col.options.select"
+                    :key="item"
+                  >
+                    {{ item }}
+                  </a-select-option>
+                </a-select>
+              </span>
+            </div>
+          </span>
           <template v-else>
             {{ text }}
           </template>
@@ -57,18 +112,24 @@ const columns = [
         },
       };
     },
+    type: "input",
   },
   {
     cnname: "年龄",
     name: "age",
     width: "25%",
     scopedSlots: { customRender: "age" },
+    type: "multiple_select",
+    options: {
+      select: ["a", "b", "c", "d", "e"],
+    },
   },
   {
     cnname: "地址",
     name: "address",
     width: "40%",
     scopedSlots: { customRender: "address" },
+    type: "input",
   },
   {
     cnname: "操作",
@@ -107,27 +168,41 @@ export default {
       });
     },
     initData() {
-      for (let i = 0; i < 100; i++) {
+      for (let i = 0; i < 800; i++) {
         this.data.push({
-          // key: i.toString(),
+          key: i.toString(),
           request_title: `Edrward ${i}`,
-          age: "",
+          age: ["aaa"],
           address: `London Park no. ${i}`,
+          disable_edit: false,
+          change_status: true,
         });
       }
       // this.cacheData = data.map((item) => ({ ...item })); //创建副本，始终为初始值
       this.cacheData = JSON.parse(JSON.stringify(this.data));
     },
     handleChange(text, value, key, column) {
-      (this.changeStyle = true),
-        //值  行  列
-        console.log("单元格更改", value, key, column);
+      // (this.changeStyle = true),
+      //值  行  列
+      console.log("单元格更改", value, key, column);
       const newData = this.data;
       const target = newData.filter((item) => key === item.key)[0];
       if (target) {
         target[column] = value;
         this.data = newData;
       }
+    },
+    selectChange(text, value, key, column) {
+      console.log("1111", text, value, key, column);
+      const newData = this.data;
+      const target = newData.filter((item) => key === item.key)[0];
+      if (target) {
+        target[column] = value;
+        this.data = newData;
+      }
+    },
+    changeEdit(record) {
+      record.change_status = !record.change_status;
     },
     save() {
       this.editInput = false;
